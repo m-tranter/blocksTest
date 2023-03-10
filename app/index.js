ccd97e9e33d829e96a472e88a8064cabc67cc181
antiloquax <tranter.m@sky.com>
2023-03-10 13:43:06 +0000

    Changed port

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 const dir = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, (error) =>{
 	if(!error)
