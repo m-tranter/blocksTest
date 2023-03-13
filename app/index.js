2d1f8376c7348cfa5e86e74f97ecd6f699c5022e
antiloquax <tranter.m@sky.com>
2023-03-13 09:17:14 +0000

    Minor - checking GitHub CI

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ const port = 3001;
 
 app.listen(port, (error) =>{
 	if(!error)
-		console.log(`Server running on port ${port}`);
+		console.log(`Server running on port ${port}.`);
 	else
 		console.log(error);
 	}
