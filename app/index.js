f15a8a3505dbe46d85e9597ad9ea09d9865e4d1f
antiloquax <tranter.m@sky.com>
2023-03-14 10:08:47 +0000

    Added minor change.

--- app/index.js
+++ app/index.js
@@ -7,8 +7,8 @@ const port = 3001;
 
 app.listen(port, (error) =>{
 	if(!error)
-		console.log(`Server running on port ${port}.`);
-		console.log(`Block running: ${new Date().toLocaleString()}`);
+		console.log(`\nServer running on port ${port}.`);
+		console.log(`\nBlock running: ${new Date().toLocaleString()}`);
 	else
 		console.log(error);
 	}
