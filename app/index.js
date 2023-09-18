514bdd02eb3d6a3e51e45c5e83ef21a827f0b212
antiloquax <tranter.m@sky.com>
2023-09-18 16:49:56 +0100

    Routing static

--- app/index.js
+++ app/index.js
@@ -87,7 +87,7 @@ async function getEntries(req, res) {
       rel="stylesheet"
     />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-      <script src="myscript.js"></script>
+      <script src="/static/myscript.js"></script>
     <title>${title}</title>
     <script type="importmap">
       {
