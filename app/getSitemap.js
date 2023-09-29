28f5a8688d182b2d2e4a82f1c00fe90e8ea78e58
antiloquax <tranter.m@sky.com>
2023-09-29 15:11:12 +0100

    Trying to avoid those 404s

--- app/getSitemap.js
+++ app/getSitemap.js
@@ -6,27 +6,30 @@ const ROOT_URL = `https://cms-chesheast.cloud.contensis.com/`;
 const PROJECT = 'website';
 
 async function getSitemap(req, res, contentType) {
-  const resp = await fetch(
+  let cf = '<changefreq>daily</changefreq>';
+  fetch(
     `${ROOT_URL}/api/delivery/projects/${PROJECT}/contenttypes/${contentType}/entries?accessToken=QCpZfwnsgnQsyHHB3ID5isS43cZnthj6YoSPtemxFGtcH15I`,
     { method: 'get' }
-  );
-
-  // Abort if no data from the CMS.
-  if (resp.status !== 200) {
-    res.sendStatus(resp.status);
-    return;
-  }
-
-  const data = await resp.json();
-  let cf = "<changefreq>daily</changefreq>";
-  let msg = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>https://www.cheshireeast.gov.uk/${contentType.toLowerCase()}/listing</loc><lastmod>${data.items[0].sys.version.modified}</lastmod>${cf}</url>`;
-  data.items.forEach((e) => {
-    msg += `<url><loc>https://www.cheshireeast.gov.uk${e.sys.uri}</loc><lastmod>${e.sys.version.modified}</lastmod>${cf}</url>`;
-  });
-  msg += '</urlset>';
-  res.set('Content-Type', 'application/xml');
-  setTimeout(() => res.send(msg), 5);
-  return;
+  )
+    .then((resp) => {
+      return resp.json();
+    })
+    .then((data) => {
+      if(!data.items.length) {
+        res.sendStatus(404);
+        return;
+      }
+      let msg = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>https://www.cheshireeast.gov.uk/${contentType.toLowerCase()}/listing</loc><lastmod>${
+        data.items[0].sys.version.modified
+      }</lastmod>${cf}</url>`;
+      data.items.forEach((e) => {
+        msg += `<url><loc>https://www.cheshireeast.gov.uk${e.sys.uri}</loc><lastmod>${e.sys.version.modified}</lastmod>${cf}</url>`;
+      });
+      msg += '</urlset>';
+      res.set('Content-Type', 'application/xml');
+      res.send(msg);
+      return;
+    });
 }
 
 export default getSitemap;
